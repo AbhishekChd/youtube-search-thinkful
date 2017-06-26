a1a3b703b4da4492ee7406f10a2ccc2d94ca17ad
AbhishekChd <acechd97@gmail.com>
2017-06-27 01:16:40 +0530

    Fixed: channels displaying with videos, now only videos are displayed

--- js/youtube-app.js
+++ js/youtube-app.js
@@ -4,13 +4,13 @@ var API_KEY = 'AIzaSyAQRN9qalqw86alca3D24iXmFJFFE8YCxw';
 var HTML_TEMPLATE = (
     '<div>'+
         '<figure>'+
-            '<a class="popup-youtube"><img></a>'+
+            '<a><img></a>'+
             '<figcaption></figcaption>'+
         '</figure>'+
     '</div>'
 );
 var modal = function(){
-     $('div  .popup-youtube').magnificPopup({
+     $('div .popup-youtube').magnificPopup({
 		disableOn: 700,
 		type: 'iframe',
 		mainClass: 'mfp-fade',
@@ -22,13 +22,18 @@ var modal = function(){
 };
 var renderItem = function(item){
     var template = $(HTML_TEMPLATE);
+
+    template.addClass('col-xs-12 col-sm-6 col-md-4 col-lg-4');
+    template.find("figure").addClass("thumbnail");
     template.find("img").attr({
         src: item.snippet.thumbnails.medium.url,
-        alt: `${item.snippet.channelTitle} thumbnail image`
+        alt: `${item.snippet.channelTitle} thumbnail image`,
+        class: 'img-responsive'
     });
-    template.find("figcaption").text(item.snippet.channelTitle);
+    template.find("figcaption").text(item.snippet.channelTitle).addClass('caption text-center');
     template.find("a").attr({
-        href: YOUTUBE_VIDEO_URL+ item.id.videoId
+        href: YOUTUBE_VIDEO_URL+ item.id.videoId,
+        class: 'popup-youtube'
     });
     return template;
 };
@@ -49,7 +54,8 @@ var getDataFromApi = function(searchTerm, callback){
             key: API_KEY,
             q: searchTerm,
             part: 'snippet',
-            maxResults: 25
+            maxResults: 25,
+            type: 'video'
         },
         type: 'GET',
         dataType: 'json',
